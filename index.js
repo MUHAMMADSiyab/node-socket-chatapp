e1680d206b90b5d59cad797ce7e636d54e0876e3
MUHAMMAD Siyab <39950367+MUHAMMADSiyab@users.noreply.github.com>
2018-06-20 21:52:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var socket = require('socket.io');
 var fs = require('fs');
-var db = require('./connection');
+//var db = require('./connection');
 
 // Setting view path
 app.use(express.static('public'));
@@ -24,18 +24,18 @@ var io = socket(server);
 // On connection
 io.on('connection', function(socket){
 
-    var chatData = [];
+//     var chatData = [];
 
-    let query = 'SELECT * FROM `chat_data`';
-    db.query(query, function(err, data){
-        if (err) {
-            console.log('Error reading data from DB');
-        } else {       
-            for (let i = 0; i < data.length; i++) {
-                chatData.push(data[i]);
-            }
-        }
-    });
+//     let query = 'SELECT * FROM `chat_data`';
+//     db.query(query, function(err, data){
+//         if (err) {
+//             console.log('Error reading data from DB');
+//         } else {       
+//             for (let i = 0; i < data.length; i++) {
+//                 chatData.push(data[i]);
+//             }
+//         }
+//     });
 
     // Listening to `user entered` event
     socket.on('user entered', function(data){
@@ -46,15 +46,15 @@ io.on('connection', function(socket){
     // Listening to `chat` event
     socket.on('chat', function(data){
 
-        // Saving chat data to database
-        let q = 'INSERT INTO `chat_data` (`id`, `sender`, `message`) VALUES (null, "' + data.username +  '","' + data.msg + '")';
-        db.query(q, function(err) {
-            if (err) {
-                console.log('Error inserting data');
-            } else {
-                console.log('data inserted');
-            }
-        });
+//         // Saving chat data to database
+//         let q = 'INSERT INTO `chat_data` (`id`, `sender`, `message`) VALUES (null, "' + data.username +  '","' + data.msg + '")';
+//         db.query(q, function(err) {
+//             if (err) {
+//                 console.log('Error inserting data');
+//             } else {
+//                 console.log('data inserted');
+//             }
+//         });
 
         // Emitting `chat` event
         io.sockets.emit('chat', data);
